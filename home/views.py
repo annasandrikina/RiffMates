9043101a4b9f3009c022807ac6d0e3192a19b544
annasandrikina <99461342+annasandrikina@users.noreply.github.com>
2025-05-22 01:09:39 +0700

    News button

--- home/views.py
+++ home/views.py
@@ -4,3 +4,10 @@ from django.http import HttpResponse
 def credits(request):
     content = "Nicky\n Anna"
     return HttpResponse (content, content_type="text/plain")
+
+def news (request):
+    data = {"news" : [
+        "Riifmates now has new page!",
+        "Riffmates has its first page "
+    ]}
+    return render (request, "news.html", data )
